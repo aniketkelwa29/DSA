edc26858025f298f5fd66726fb2477be3f636b40
aniketkelwa29 <166652870+aniketkelwa29@users.noreply.github.com>
2026-01-22 20:57:57 +0530

    correct the program formate

--- src/DSA_Projects/P1_Restraunt_Management.java
+++ src/DSA_Projects/P1_Restraunt_Management.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 class TokenSystem {
+
     int tokenNumber;
     TokenSystem front = null;
     TokenSystem rear = null;
@@ -16,24 +17,30 @@ class TokenSystem {
     }
 
     void orderRecieved(int tokenNumber) {
+
         TokenSystem newNode = new TokenSystem(tokenNumber);
-//        System.out.println("test1");
         count++;
+
         if (front == null) {
             front = newNode;
             rear = newNode;
-            System.out.println("New Order recieved !! \n  serve at the table no. " + tokenNumber);
+
+            System.out.println("New Order recieved !!");
+            System.out.println("serve at the table no. " + tokenNumber);
             System.out.println("number of orders to serve are  : " + count);
             return;
         }
+
         rear.next = newNode;
         rear = newNode;
 
-        System.out.println("New Order recieved !! \n  serve at the table no. " + tokenNumber);
+        System.out.println("New Order recieved !!");
+        System.out.println("serve at the table no. " + tokenNumber);
         System.out.println("number of orders to serve are  : " + count);
     }
 
     void orderServed() {
+
         if (front == null && rear == null) {
             System.out.println("Not any order is pending right now : ");
         }
@@ -48,7 +55,10 @@ class TokenSystem {
     }
 }
 
+/* ---------------- ORDER HISTORY ---------------- */
+
 class OrderHistory {
+
     int Data;
     int top = 0;
     OrderHistory next;
@@ -60,32 +70,39 @@ class OrderHistory {
     }
 
     void History(int Data) {
+
         OrderHistory newBill = new OrderHistory(Data);
         newBill.Data = Data;
         newBill.next = head;
         head = newBill;
         top++;
+
         getHistory();
     }
 
     void getHistory() {
+
         OrderHistory current = head;
+
         while (current != null) {
-            System.out.println("Most Recent bill  is : "+current.Data);
+            System.out.println("Most Recent bill  is : " + current.Data);
             current = current.next;
         }
     }
-
 }
+
+/* ---------------- DISHES ---------------- */
 
 class Dishes {
 
     OrderHistory orderHistory = new OrderHistory(0);
     int orderId = 1000;
-      TokenSystem tokenSystem =  new TokenSystem(orderId)  ;
+    TokenSystem tokenSystem = new TokenSystem(orderId);
+
     HashMap<String, Integer> breakfast = new HashMap<>();
     HashMap<String, Integer> lunch = new HashMap<>();
     HashMap<String, Integer> dinner = new HashMap<>();
+
     Scanner sc = new Scanner(System.in);
 
     public Dishes() {
@@ -99,11 +116,9 @@ class Dishes {
         breakfast.put("Poha", 25);
         breakfast.put("Samosa", 25);
 
-
         lunch.put("Veg Thali", 80);
         lunch.put("Daal Rice", 65);
         lunch.put("Paneer curry", 109);
-
 
         dinner.put("sabzi & chapati", 70);
         dinner.put("Fried rice", 65);
@@ -111,179 +126,207 @@ class Dishes {
     }
 
     public void breakfastMenu() {
+
         System.out.println("----------BREAKFAST MENU-----------");
+
         for (Map.Entry<String, Integer> n : breakfast.entrySet()) {
             System.out.println(n.getKey() + " -> " + n.getValue());
         }
     }
 
     public void lunchMenu() {
+
         System.out.println("----------LUNCH MENU-----------");
+
         for (Map.Entry<String, Integer> m : lunch.entrySet()) {
             System.out.println(m.getKey() + " -> " + m.getValue());
         }
     }
 
     public void dinnerMenu() {
+
         System.out.println("----------DINNER MENU-----------");
+
         for (Map.Entry<String, Integer> o : dinner.entrySet()) {
             System.out.println(o.getKey() + " -> " + o.getValue());
         }
     }
 
-
     public void orders() {
+
         HashMap<String, Integer> order = new HashMap<>();
+
         System.out.println("Enter the number of items you want to order : ");
         int n = sc.nextInt();
         sc.nextLine();
 
         for (int i = 0; i < n; i++) {
+
             System.out.println("Enter the name of the dish : ");
             String dish = sc.nextLine().trim();
+
             System.out.println("Enter quantity : ");
             int quantity = sc.nextInt();
             sc.nextLine();
 
             order.put(dish, order.getOrDefault(dish, 0) + quantity);
-
         }
+
         orderId++;
         tokenSystem.orderRecieved(orderId);
+
         for (Map.Entry<String, Integer> entry : order.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
-        generateBill(order);
 
+        generateBill(order);
     }
 
-
-
     public void generateBill(HashMap<String, Integer> bill) {
+
         int grandTotal = 0;
+
         System.out.println("-----------BILL------------");
+
         for (Map.Entry<String, Integer> item : bill.entrySet()) {
+
             String dish = item.getKey();
             int quantity = item.getValue();
+
             if (breakfast.containsKey(dish)) {
+
                 int price = breakfast.get(dish);
                 int total = price * quantity;
                 grandTotal += total;
+
                 System.out.println(dish + " | ₹" + price + " x " + quantity + " = ₹" + total);
+
             } else if (lunch.containsKey(dish)) {
+
                 int price = lunch.get(dish);
                 int total = price * quantity;
                 grandTotal += total;
+
                 System.out.println(dish + " | ₹" + price + " x " + quantity + " = ₹" + total);
+
             } else if (dinner.containsKey(dish)) {
+
                 int price = dinner.get(dish);
                 int total = price * quantity;
                 grandTotal += total;
-                System.out.println(dish + " | ₹" + price + " x " + quantity + " = ₹" + total);
-            } else {
 
+                System.out.println(dish + " | ₹" + price + " x " + quantity + " = ₹" + total);
+
+            } else {
                 System.out.println(dish + " : Not Available");
             }
         }
+
         System.out.println("-----------------------------");
         System.out.println("Grand Total: ₹" + grandTotal);
-          tokenSystem.orderServed();
+
+        tokenSystem.orderServed();
+
         System.out.println("Do you want to see the bill history ? (yes/no) ");
         String choice = sc.nextLine();
-        if(choice.equals("yes"))
-          orderHistory.History(grandTotal);
-        else if (choice.equals("no")){
+
+        if (choice.equals("yes")) {
+            orderHistory.History(grandTotal);
+        } else if (choice.equals("no")) {
             System.out.println("!! Thankyou for having a meal !!");
         }
-//        restraunt.Reception();
     }
-
 }
 
+/* ---------------- RESTAURANT ---------------- */
+
 class Restraunt {
+
     Dishes dishes = new Dishes();
     Scanner sc = new Scanner(System.in);
 
-    public void orderConfirmation()
-    {
+    public void orderConfirmation() {
+
         System.out.println("Do you want to order something from this menu  :- yes/no ");
         String m = sc.nextLine();
-        if (m.equals("yes"))
-        {
 
+        if (m.equals("yes")) {
             dishes.orders();
-
-        }
-        else if (m.equals("no"))
-        {
+        } else if (m.equals("no")) {
             System.out.println("!! Thankyou for visiting !!");
-        }
-        else
-        {
+        } else {
             System.out.println("!! Invalid Input !!");
         }
     }
-    public void Reception()
-    {
+
+    public void Reception() {
 
         Restraunt restraunt = new Restraunt();
         System.out.println("!!----Welcome to our Restraunt----!!");
 
-
         Boolean b = false;
+
         do {
+
             System.out.println("Do you wants to have a meal :  yes/no : ");
             String userChoice = sc.nextLine().trim();
-            if (userChoice.equals("yes"))
-            {
+
+            if (userChoice.equals("yes")) {
 
                 Boolean r = false;
+
                 do {
-                    System.out.println("Which type of meal do you prefer  " +
-                            "\n ----------BREAKFAST----------- " +
-                            "\n " + "----------LUNCH----------- " +
-                            "\n" + "----------DINNER-----------  ");
+
+                    System.out.println(
+                            "Which type of meal do you prefer  " +
+                                    "\n ----------BREAKFAST----------- " +
+                                    "\n ----------LUNCH----------- " +
+                                    "\n ----------DINNER-----------"
+                    );
+
                     String c = sc.nextLine();
-                    if (c.equals("breakfast"))
-                    {
+
+                    if (c.equals("breakfast")) {
                         dishes.breakfastMenu();
                         restraunt.orderConfirmation();
                         r = true;
-                    }
-                    else if (c.equals("lunch"))
-                    {
+
+                    } else if (c.equals("lunch")) {
                         dishes.lunchMenu();
                         orderConfirmation();
                         r = true;
-                    }
-                    else if (c.equals("dinner"))
-                    {
+
+                    } else if (c.equals("dinner")) {
                         dishes.dinnerMenu();
                         orderConfirmation();
                         r = true;
-                    }
-                    else
-                    {
+
+                    } else {
                         System.out.println("! Invalid Input !");
                     }
+
                 } while (r != true);
+
                 b = true;
-            }
-            else if (userChoice.equals("no"))
-            {
+
+            } else if (userChoice.equals("no")) {
+
                 System.out.println("!! Thankyou - visit Again !!");
                 b = true;
-            }
-            else
-            {
+
+            } else {
                 System.out.println("userInput is not valid ");
             }
+
         } while (b != true);
     }
 }
+/* ---------------- MAIN ---------------- */
 
 public class P1_Restraunt_Management {
+
     public static void main(String[] args) {
+
         Restraunt r = new Restraunt();
         r.Reception();
     }
